b752155b2583a5a7ac87f12dc386854c2171d56e
richard-mcghee <richardjmcghee@yahoo.com>
2020-08-06 16:18:01 -0400

    created a couple of tests in cForms test, they're working as intended and can be failed easily

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,3 +5,14 @@ import { render } from '@testing-library/react'
 test("renders ContactForm without crashing", () => {
     render(<ContactForm />);
   });
+
+test("finds First Name Input", () => {
+    const { getByText } = render(<ContactForm />)
+    const firstNameForm = getByText(/first name/i)
+    expect(firstNameForm).toBeInTheDocument()
+})
+test("Finds First Name placeholder text", ( () => {
+    const { getByPlaceholderText } = render(<ContactForm />)
+    const fnamePHText = getByPlaceholderText(/edd/i)
+    expect(fnamePHText).toBeInTheDocument()
+}))
